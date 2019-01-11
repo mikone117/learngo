ce240aabeeac11681e55cec790d26751faa68e3f
Miguel Medina <miguel.medina@skimlinks.com>
2019-01-11 12:56:33 +0000

    feat: add comments example

---
+++ first/comments/main.go
@@ -0,0 +1,15 @@
+// This is an executable program
+package main
+
+import (
+	"fmt"
+)
+
+/*
+main function
+Go executes thus program using this function
+There should be only one maun file un a main package
+*/
+func main() {
+	fmt.Println("Hello gopher!", value)
+}
