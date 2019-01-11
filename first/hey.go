ff2b57514896fcb593da09c45e4e28a845a12447
Miguel Medina <miguel.medina@skimlinks.com>
2019-01-11 12:46:03 +0000

    feat: use of 'fmt' and Hello World

---
+++ first/hey.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+const value int8 = 10
+
+func hey() {
+	fmt.Println("Hey oh! Let's GO!")
+}
--- first/main.go
+++ first/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello gopher!")
+	fmt.Println("Hello gopher!", value)
+	bye()
+	hey()
 }
