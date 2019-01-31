b0539d7993414ea9953463f40d51c5aeea87c82f
Miguel Medina <miguel.medina@skimlinks.com>
2019-01-31 12:26:50 +0000

    feat: add zero values example

---
+++ first/zero_values/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var count int
+	var inches float64
+	var on bool
+	var name string
+
+	fmt.Println(count)
+	fmt.Println(inches)
+	fmt.Println(on)
+	fmt.Printf("%q\n", name)
+}
