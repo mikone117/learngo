bee1e83c9b15832db1cfbce77e1799ab5b534a05
Miguel Medina <miguel.medina@skimlinks.com>
2019-12-03 11:50:12 +0000

    feat: add small changes

--- first/celsius_to_fahrenheit/main.go
+++ first/celsius_to_fahrenheit/main.go
@@ -4,11 +4,23 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
+const errorMessage = "Usage: go run main.go <celsius number>"
+
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println(strings.TrimSpace(errorMessage))
+		return
+	}
 	arg := os.Args[1]
-	celsius, _ := strconv.ParseFloat(arg, 64)
+	celsius, err := strconv.ParseFloat(arg, 64)
+
+	if err != nil {
+		fmt.Printf("error: %q is not a number\n", arg)
+		return
+	}
 
 	fahrenheit := celsius*1.8 + 32
 
